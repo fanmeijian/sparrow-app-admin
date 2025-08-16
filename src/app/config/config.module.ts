ca04973a09b6e7ebca433bc3ff553bc6adc80693
Fan Sword <fanmeijian@gmail.com>
2025-08-17 03:35:27 +0800

    完成了，安心睡吧

--- src/app/config/config.module.ts
+++ src/app/config/config.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigListComponent } from './config-list/config-list.component';
 import { ConfigRoutingModule } from './config-routing.module';
+import { AngularMaterialModule } from '../angular-material.module';
 
 
 
@@ -12,6 +13,7 @@ import { ConfigRoutingModule } from './config-routing.module';
   imports: [
     CommonModule,
     ConfigRoutingModule,
+    AngularMaterialModule,
   ]
 })
 export class ConfigModule { }
